7a99d1f76c9edef6ecbc38601c38da91cf85f99f
xMari <xmaribr@gmail.com>
2017-10-28 09:13:14 -0200

    working on Login with E-mail
    working on Login with E-mail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,54 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import {AngularFireModule} from 'angularfire2';
+import { AngularFireAuthModule} from 'angularfire2/auth';
+
 import { MyApp } from './app.component';
+
 import { HomePage } from '../pages/home/home';
+import { ResetpasswordPage } from '../pages/resetpassword/resetpassword';
+import { SigninPage } from '../pages/signin/signin';
+import { SignupPage } from '../pages/signup/signup';
+
+import { AuthService } from '../providers/auth/auth-service';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6UaRs-fuANSLU9CJw3gHXCjq2AeOtM4w",
+  authDomain: "resolutions-9cfae.firebaseapp.com",
+  databaseURL: "https://resolutions-9cfae.firebaseio.com",
+  projectId: "resolutions-9cfae",
+  storageBucket: "resolutions-9cfae.appspot.com",
+  messagingSenderId: "764041532194"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ResetpasswordPage,
+    SigninPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ResetpasswordPage,
+    SigninPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthService } from '../../providers/auth/auth-service';
+import { SigninPage } from '../signin/signin';
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private authService: AuthService) {
   }
 
+  signOut() {
+    this.authService.signOut()
+      .then(() => {
+        this.navCtrl.setRoot(SigninPage);
+      })
+      .catch((error)=> {
+        console.error(error);
+      });
+  }
 }
